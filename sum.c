37b98b9d77e4b9ccae5f7d890a25c712af044079
QiushiZhou <qiushi.zhou@qq.com>
2019-03-27 10:17:59 -0500

    :

--- sum.c
+++ sum.c
@@ -10,7 +10,8 @@ input: intger number
 
 main(){
   //declear variables
-  int i,num,sum=0;
+  int i,num,sum=0,count;
+  float averg=0.0;
   //promote the user from num
   printf("please enter the value of the number : \n");
   scanf("%d",&num);
@@ -19,10 +20,11 @@ main(){
   for(i=1;i<=num;i++)
     {
       sum+=i;
+      count=i;
     }
   //print the sum
   printf("The sum of the numbers from 1 to num= %d is sum=%d  \n",num,sum);
-
+  printf("the average of the number is : %f \n",sum/count);
 
   return 0;
 }//end of main
